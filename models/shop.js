561f9d32f317d66744d6b3f4e3cb41a719e52631
Blinov Maksim <tenzm@mail.ru>
2019-06-05 12:23:04 +0300

    connect shop to db

--- models/shop.js
+++ models/shop.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const shopSchema = new Schema({
-  id: {
-      required: true,
-      unique: true,
-      type: Number,
-  },
   name: String,
   type: String,
   options: {},
